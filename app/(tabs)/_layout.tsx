f60f7c4488643e161c84533a043f5727577df764
DevTech <micahkesiena@gmail.com>
2026-03-29 18:49:10 +0100

    feat: tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,9 @@ export default function TabLayout() {
     <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
       <Tabs.Screen name="chat" options={{ title: "Chat" }} />
+      <Tabs.Screen name="createNew" options={{ title: "Create New" }} />
+      <Tabs.Screen name="inbox" options={{ title: "Inbox" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
     </Tabs>
   );
 }
