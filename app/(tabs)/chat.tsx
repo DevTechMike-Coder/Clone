e26585b9316ac25b53c0a13883163f310b7ce134
DevTech <micahkesiena@gmail.com>
2026-03-30 20:27:11 +0100

    feat: styling the pages and add necessary redirects

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,10 +1,37 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+import { router } from "expo-router";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Chat() {
   return (
-    <SafeAreaView>
-      <Text>DM for More</Text>
+    <SafeAreaView className="flex-1 p-5">
+      <View className="flex-row items-center justify-between">
+        <TouchableOpacity
+          onPress={() => router.push("/followpage")}
+          activeOpacity={0.7}
+        >
+          <Image
+            source={require("@/assets/homeIcons/add-user.png")}
+            className="w-10 h-10"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+        <Text className="text-xl font-semibold tracking-tight">Chat</Text>
+        <Image
+          source={require("@/assets/homeIcons/menuV.png")}
+          className="w-10 h-10"
+          resizeMode="contain"
+        />
+      </View>
+
+      <View className="py-7 px-4">
+        <Text className="text-xl font-semibold tracking-tight">
+          DM for More
+        </Text>
+      </View>
     </SafeAreaView>
   );
 }
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,13 @@
 import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Profile() {
-    return (
-        <SafeAreaView>
-            <Text>Profile</Text>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView className="flex-1 p-5">
+      <Text className="text-2xl font-semibold text-center py-4">Profile</Text>
+    </SafeAreaView>
+  );
 }
